3ec17c373c8c3e233cc42bab07d713a7a2aeed2a
julian camilo sanchez rojas <jcsanchez4050@misena.edu.co>
2022-06-14 08:57:47 -0500

    video36

--- flutter_app/lib/login/login_summary.dart
+++ flutter_app/lib/login/login_summary.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class LoginSummary extends StatelessWidget {
@@ -6,6 +8,63 @@ class LoginSummary extends StatelessWidget {
   LoginSummary({this.horizontal});
   @override
   Widget build(BuildContext context) {
-    return;
+    final imageThumbnail = Container(
+      margin: EdgeInsets.symmetric(vertical: 16.0),
+      alignment:horizontal ? FractionalOffset.centerLeft : FractionalOffset.center,
+      child: Container(height: 90.0,
+      width: 90.0
+      decoration: BoxDecoration(color: Colors.black26,
+      shape: BoxShape.rectangle
+      borderRadius:BorderRadius.circular(50.0),
+      image: DecorationImage( 
+      image: AssetImage("assets/logo-google.png"),
+      fit: BoxFit.cover
+      ),
+      boxShadow:[
+        BoxShadow(
+          color:Color(0xffA4A4A4),
+          offset:Offset(1.0,2.0),
+          blurRadius:3.0,
+           ),
+      ],
+      ),
+      ),
+    );
+
+    final userCardContent = Container(
+      margin: EdgeInsets.fromLTRB(horizontal ? 76.0:16.0, horizontal ? 16.0:41.0,16.0,16.0),
+      constraints: BoxConstraints.expand(),
+      child: Column(
+        crossAxisAlignment: horizontal ? CrossAxisAlignment.start:CrossAxisAlignment.center,
+        children:[
+          Container(
+            height:4.0),
+           Text("Login",
+            style:TextStyle(fontSize:20,
+            color:Colors.white,
+            ),
+            ) ,
+      ],
+      ),
+    );
+    final usertCard =Container(
+      child: userCardContent,
+      height: horizontal ? 124.0 : 154.0,
+      margin: horizontal
+         ? EdgeInsets.only(left: 46.0)
+         : EdgeInsets.only(top: 72.0),
+      decoration: BoxDecoration(
+        color: Colors.black54,
+        shape: BoxShape.rectangle
+        borderRadius:BorderRadius.circular(8.0),
+          boxShadow:<BoxShadow>[
+            BoxShadow(
+              color:Color(0xffA4A4A4),
+              blurRadius:5.0,
+              offset:Offset(0.0,2.0),
+           ),
+      ],
+      ),
+    );
   }
 }
